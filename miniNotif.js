877606b74f3938d81dbc8844adb7124ef79e97bf
TheRolf <d75ful@live.fr>
2017-11-25 12:33:41 +0100

    Create miniNotif.js

---
+++ miniNotif.js
@@ -0,0 +1,30 @@
+var miniNotif = {
+	//init
+	init: function(){
+    if(!$('#miniNotif').length) {
+        $('body').append('<div id="miniNotif"></div>');
+    }
+	},
+	
+	//add notif
+	addNotif: function(proccessOrNot, text, icon = '', color ='') {
+		var fontcolor = 'black' || color; // default color is white
+		
+		var notif = $('<div class="miniNotif" style="color: ' + fontcolor + '"><div class="icon">' + icon + '</div><div> ' + text + '</div></div>').appendTo($('body').find('#miniNotif')).css('opacity', '0').animate({ opacity: '1' }, 300).delay(500);
+		
+		//then i proccess my notif according to thee var
+		if(!proccessOrNot) {
+			return notif;
+		} else {
+			// get out
+			miniNotif.done(notif);
+		}
+	},
+	
+	// proccess function
+	done: function(notif) {
+			notif.animate({
+					right: '-500px',
+			}, 300).delay(300).slideUp(300);
+	}
+}
